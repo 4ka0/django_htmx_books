1ff3a56c6582fe6909c6146e7953c755cd9a982e
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-01-10 12:28:33 +0900

    Redundant model code removed

--- books/models.py
+++ books/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.urls import reverse
 
 
 class Book(models.Model):
@@ -10,6 +9,3 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse("book_detail", args=[str(self.id)])
