cc3bb06f26c850b8f4a5f356e72063f59eb6bbc9
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-01-15 10:20:48 +0900

    Code cleanup

--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_http_methods
-from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Book
 from .forms import BookForm
