2a170cfa8448d10117b5390dd2ef3da4ab141210
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-01-17 22:48:22 +0900

    Altered book price model field type

--- books/models.py
+++ books/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.PositiveIntegerField(default=0)
     read = models.BooleanField(default=False)
 
     def __str__(self):
